83816c6e1e073692a1ab659ab3fff6d3ef1fe6e4
nzoellner1384 <nzoellner1384@gmail.com>
2017-09-27 13:26:15 -0500

    Fix

--- src/ciphergit/FXMLDocumentController.java
+++ src/ciphergit/FXMLDocumentController.java
@@ -10,17 +10,44 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 
 /**
+ * FXML Controller class
  *
  * @author nzoel
  */
 public class FXMLDocumentController implements Initializable {
-    
+
+    @FXML
+    private TextArea plainTextField;
+    @FXML
+    private Button cipherButton;
+    @FXML
+    private RadioButton encryptRadioButton;
+    @FXML
+    private ToggleGroup RadioButtons;
+    @FXML
+    private RadioButton decryptRadioButton;
+    @FXML
+    private TextField seedTextBox;
+    @FXML
+    private TextArea cipheredTextField;
+
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void cipherButton_Click(ActionEvent event) {
+    }
     
 }
