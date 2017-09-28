04523bf714a0fee235862e2ffe6e002a050cb32d
nzoellner1384 <nzoellner1384@gmail.com>
2017-09-28 11:06:00 -0500

    Finished Cipher Class

--- src/ciphergit/Cipher.java
+++ src/ciphergit/Cipher.java
@@ -16,43 +16,59 @@ public class Cipher {
     
     private String alphabet = "abcdefghijklmnopqrstuvwxyz";
     private String cipher = "";
-    private String phrase;
+    private String phrase = "";
     private String endPhrase = "";
     
     Cipher (int seed, String phrase, char type) {
+        
         Random rand = new Random(seed);
         this.phrase = phrase;
         int count = 0;
         int temp;
         
         while (count < alphabet.length()) {
+            
             temp = rand.nextInt(alphabet.length());
+            
             if (cipher.indexOf(alphabet.charAt(temp)) < 0) {
+                
                 cipher += alphabet.charAt(temp);
                 count++;
+                
             }
         }
         
         System.out.println(this.cipher);
         
         if (type == 'e') {
+            
             encrypt();
+            
         } else if (type == 'd') {
+            
             decrypt();
+            
         }
     }
     
     private void encrypt() {
+        
         for (int i = 0; i < phrase.length(); i++) {
+            
             if (phrase.charAt(i) != ' ' || !Character.isDigit(phrase.charAt(i))) {
+                
                 if (Character.isUpperCase(phrase.charAt(i))) {
-                    alphabet.toUpperCase();
-                    cipher.toUpperCase();
-                    endPhrase += cipher.charAt(alphabet.indexOf(phrase.charAt(i)));
+                    
+                    endPhrase += cipher.toUpperCase().charAt(alphabet.toUpperCase().indexOf(phrase.charAt(i)));
+                    
                 } else if (Character.isLowerCase(phrase.charAt(i))) {
-                    alphabet.toLowerCase();
-                    cipher.toLowerCase();
+                    
                     endPhrase += cipher.charAt(alphabet.indexOf(phrase.charAt(i)));
+                    
+                } else {
+                    
+                    endPhrase += phrase.charAt(i);
+                    
                 }
                 
             }
@@ -60,15 +76,38 @@ public class Cipher {
     }
     
     private void decrypt() {
-        
+        for (int i = 0; i < phrase.length(); i++) {
+            
+            if (phrase.charAt(i) != ' ' || !Character.isDigit(phrase.charAt(i))) {
+                
+                if (Character.isUpperCase(phrase.charAt(i))) {
+                    
+                    endPhrase += alphabet.toUpperCase().charAt(cipher.toUpperCase().indexOf(phrase.charAt(i)));
+                    
+                } else if (Character.isLowerCase(phrase.charAt(i))) {
+                    
+                    endPhrase += alphabet.charAt(cipher.indexOf(phrase.charAt(i)));
+                    
+                } else {
+                    
+                    endPhrase += phrase.charAt(i);
+                    
+                }
+                
+            }
+        }
     }
     
     public String getEncryptedPhrase() {
+        
         return endPhrase;
+        
     }
     
     public String getDecryptedPhrase() {
+        
         return endPhrase;
+        
     }
     
 }
